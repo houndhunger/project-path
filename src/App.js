7e885a510040d59e6b8babf50c943fc3bb0ac410
Daniel Pribula <daniel.pribula@gmail.com>
2024-11-09 16:04:01 +0000

    Project setup - first logo design, Bootstrap NavBar & CSS module setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css'
+//import Button from 'react-bootstrap/Button';
+import NavBar from './components/NavBar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>Hello, World!</h1>
-      </header>
+    <div className={styles.App}>
+      <NavBar />
     </div>
   );
 }
