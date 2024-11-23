6776401205323c744aefabc8a546d585dff63737
Daniel Pribula <daniel.pribula@gmail.com>
2024-11-23 22:19:42 +0000

    add axios interceptors

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -28,7 +28,7 @@ const NavBar = () => {
       </NavLink>
     </>
   );
-  
+
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
@@ -47,20 +47,6 @@ const NavBar = () => {
               to="/"
             >
               <i className="fas fa-home"></i> Home
-            </NavLink>
-            <NavLink
-              className={styles.NavLink}
-              activeClassName={styles.Active}
-              to="/signin"
-            >
-              <i className="fas fa-sign-in-alt"></i> Sign in
-            </NavLink>
-            <NavLink
-              className={styles.NavLink}
-              activeClassName={styles.Active}
-              to="/signup"
-            >
-              <i className="fas fa-user-plus"></i> Sign up
             </NavLink>
 
             {currentUser ? loggedInIcons : loggedOutIcons}
