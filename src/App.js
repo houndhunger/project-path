1856d490a7b732d2b84080a09b0f9fee74a2cf50
Daniel Pribula <daniel.pribula@gmail.com>
2024-11-29 22:44:39 +0000

    PostsPage - Home, Feed, Liked function properly

--- src/App.js
+++ src/App.js
@@ -1,43 +1,50 @@
-import { Container } from 'react-bootstrap';
-import styles from './App.module.css'
-import NavBar from './components/NavBar';
-import {Route, Switch} from 'react-router-dom'
-import './api/axiosDefaults'
-import SignUpForm from './pages/auth/SignUpForm';
-import SignInForm from './pages/auth/SignInForm';
-import PostCreateForm from './pages/posts/PostCreateForm';
+import { Container } from "react-bootstrap";
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import { Route, Switch } from "react-router-dom";
+import "./api/axiosDefaults";
+import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
+import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
-import PostsPage from './pages/posts/PostsPage';
-import { useCurrentUser } from './contexts/CurrentUserContext';
-
+import PostsPage from "./pages/posts/PostsPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
-  const  currentUser = useCurrentUser();
+  const currentUser = useCurrentUser();
   const profile_id = currentUser?.profile_id || "";
-
 
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => (
-            <PostsPage message="No results are found. Adjust the search keyword." />
-          )}
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <PostsPage message="No results are found. Adjust the search keyword." />
+            )}
           />
-          <Route exact path="/feed" render={() => (
-            <PostsPage 
-              message="No results are found. Adjust the search keyword or follow a user."
-              filter={`owner__followed__owner__profile=${profile_id}&`} 
-            />
-          )}
+          <Route
+            exact
+            path="/feed"
+            render={() => (
+              <PostsPage
+                message="No results are found. Adjust the search keyword or follow a user."
+                filter={`owner__followed__owner__profile=${profile_id}&`}
+              />
+            )}
           />
-          <Route exact path="/liked" render={() => (
-            <PostsPage 
-              message="No results are found. Adjust the search keyword or like the post."
-              filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
-            />
-          )}
+          <Route
+            exact
+            path="/liked"
+            render={() => (
+              <PostsPage
+                message="No results are found. Adjust the search keyword or like the post."
+                filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
+              />
+            )}
           />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
@@ -45,7 +52,7 @@ function App() {
           <Route exact path="/posts/:id" render={() => <PostPage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
-    </Container>
+      </Container>
     </div>
   );
 }
