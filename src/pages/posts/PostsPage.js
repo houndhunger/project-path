e551a54d7eb7980f5fab2945a3c8b300f2801d9b
Daniel Pribula <daniel.pribula@gmail.com>
2024-12-01 00:24:55 +0000

    add Comments Component to the Post

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -17,12 +17,19 @@ import NoResults from "../../assets/no-results.png";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
 
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
 function PostsPage({ message, filter = "" }) {
     const [posts, setPosts] = useState({ results: [] });
     const [hasLoaded, setHasLoaded] = useState(false);
     const { pathname } = useLocation();
   
     const [query, setQuery] = useState("");
+
+    const currentUser = useCurrentUser();
+    const profile_image = currentUser?.profile_image;
+    const [comments, setComments] = useState({ results: [] });
 
     useEffect(() => {
       const fetchPosts = async () => {
