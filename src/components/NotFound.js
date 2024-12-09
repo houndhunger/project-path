b3a4feb5de9eb0577fdf5a58c10ff7c93cbd7411
Daniel Pribula <daniel.pribula@gmail.com>
2024-12-09 22:36:16 +0000

    feat: add NotFound page

---
+++ src/components/NotFound.js
@@ -0,0 +1,24 @@
+import React from "react";
+import NoResults from "../assets/not-found.png";
+import styles from "../styles/NotFound.module.css";
+import Asset from "./Asset";
+
+const NotFound = () => {
+  return (
+    <div className={styles.NotFound}>
+      <Asset
+        src={NoResults}
+        message={
+          <p className="text-center">
+            Architecture may strive to be timeless, yet even it cannot escape the relentless flow of time.
+            <br />
+            <br />
+            Unfortunately, the page you're looking for seems to have been swept away.
+          </p>
+        }
+      />
+    </div>
+  );
+};
+
+export default NotFound;
