b8d93f3ff99f5860feed211abadda666aac07790
Daniel Pribula <daniel.pribula@gmail.com>
2024-12-14 21:44:00 +0000

    feat: add Notifications NavLink item

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -56,6 +56,13 @@ const NavBar = () => {
       >
         <i className="fas fa-heart"></i>Liked
       </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/notifications"
+      >
+        <i className="fas fa-bell"></i>Notifications
+      </NavLink>
       <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
         <i className="fas fa-sign-out-alt"></i>Sign out
       </NavLink>
